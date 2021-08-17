57acb26f59b35cbbd87b1c05372581a7e73f02b2
daegontaven <8206808+daegontaven@users.noreply.github.com>
2021-08-18 00:30:50 +0530

    Add Registration Page

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="login">
-    <div class="login_box">
+  <div class="register">
+    <div class="register_box">
       <n-form :label-width="80" :model="model" :rules="rules" ref="form">
         <n-card>
           <n-space vertical>
@@ -19,20 +19,32 @@
                 @keydown.enter.prevent
               />
             </n-form-item>
+            <n-form-item first path="reEnteredPassword" ref="rPasswordFormItem">
+              <n-input
+                :disabled="!model.password"
+                v-model:value="model.reEnteredPassword"
+                type="password"
+                placeholder="Re-enter Password"
+                @keydown.enter.prevent
+              />
+            </n-form-item>
             <n-space justify="space-between">
               <n-button
+                :disabled="
+                  !model.username || !model.password || !model.reEnteredPassword
+                "
                 type="primary"
                 icon-placement="left"
-                @click="handleValidateButtonClick"
+                @click="handleRegisterButtonClick"
               >
                 <template #icon>
                   <n-icon>
                     <login-icon />
                   </n-icon>
                 </template>
-                Login
+                Register
               </n-button>
-              <n-button dashed @click="$router.push('register')">Register</n-button>
+              <n-button dashed @click="$router.push('login')">Login</n-button>
             </n-space>
           </n-space>
         </n-card>
@@ -55,6 +67,7 @@ import {
   NForm,
   NFormItem,
   FormInst,
+  FormItemInst,
 } from "naive-ui";
 import { Login as LoginIcon } from "@vicons/tabler";
 
@@ -73,15 +86,28 @@ export default defineComponent({
   },
   setup() {
     let loggedIn = ref(true);
-    let username = ref("");
-    let password = ref("");
+
+    const rPasswordFormItem = ref<FormItemInst | null>(null);
 
     const form = ref<FormInst | null>(null);
 
-    let model = ref({
+    let model = ref<Record<string, null | string>>({
       username: null,
       password: null,
+      reEnteredPassword: null,
     });
+
+    function validatePasswordStartWith(rule: FormItemRule, value: string) {
+      return (
+        model.value.password &&
+        model.value.password.startsWith(value) &&
+        model.value.password.length >= value.length
+      );
+    }
+
+    function validatePasswordSame(rule: FormItemRule, value: string) {
+      return value === model.value.password;
+    }
 
     let rules = ref({
       username: [
@@ -102,9 +128,34 @@ export default defineComponent({
           message: "Password is required.",
         },
       ],
+      reEnteredPassword: [
+        {
+          required: true,
+          message: "Re-entered Password is required",
+          trigger: ["input", "blur"],
+        },
+        {
+          validator: validatePasswordStartWith,
+          message: "Password is not same as re-entered password!",
+          trigger: "input",
+        },
+        {
+          validator: validatePasswordSame,
+          message: "Password is not same as re-entered password!",
+          trigger: ["blur", "password-input"],
+        },
+      ],
     });
 
-    function handleValidateButtonClick(e: ErrorEvent) {
+    function handlePasswordInput() {
+      if (model.value.reEnteredPassword) {
+        if (rPasswordFormItem.value) {
+          rPasswordFormItem.value.validate({ trigger: "password-input" });
+        }
+      }
+    }
+
+    function handleRegisterButtonClick(e: ErrorEvent) {
       e.preventDefault();
       if (form.value) {
         form.value.validate((errors: unknown) => {
@@ -117,19 +168,19 @@ export default defineComponent({
 
     return {
       loggedIn,
-      username,
-      password,
+      rPasswordFormItem,
       form,
-      model,
+      model: model,
       rules,
-      handleValidateButtonClick,
+      handlePasswordInput,
+      handleRegisterButtonClick,
     };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.login_box {
+.register {
   display: flex;
   justify-content: center;
   align-items: center;
